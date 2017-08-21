a5cdd7c419c98c9e319c76fe7cc6218407c3705b
cgreene19 <cid.greene@gmail.com>
2017-08-20 22:31:04 -0500

    added ts.d for vue files and added a new details.vue

---
+++ app/components/details.vue
@@ -0,0 +1,17 @@
+<!-- app/components/details.vue -->
+<template>
+    <h2>
+        {{msg}}</h2>
+</template>
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({   
+    data(){
+return{
+    msg: "Hello!"
+}
+
+    }
+});
+</script>
+<style></style>
